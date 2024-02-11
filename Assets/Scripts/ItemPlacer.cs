6278e41af2fda830741d6f0e90025d04f4f2b439
RicardoAviles951 <90577347+RicardoAviles951@users.noreply.github.com>
2024-02-10 19:33:03 -0800

    Updated Ink Stuff

--- Assets/Scripts/ItemPlacer.cs
+++ Assets/Scripts/ItemPlacer.cs
@@ -1,12 +1,17 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemPlacer : MonoBehaviour, IPlacer
 {
+    public event Action AllItemsPlaced;
     public List<Transform> transformList = new List<Transform>();
     public int itemCount = 0;
+    public int maxItemCount;
+    public AK.Wwise.Event itemPlaceSound;
+    public AK.Wwise.Event allItemsPlacedSound;
 
     public void PlaceItem(IExaminable i)
     {
@@ -17,20 +22,29 @@ public class ItemPlacer : MonoBehaviour, IPlacer
             i.ToggleExaminable(true, transformList[itemCount].position);
             //set flag so the object cannot be interacted with again. 
             i.isGrabbable = false;
-
+            //Play Sound
+            itemPlaceSound.Post(gameObject);
             itemCount++;
         }
         else //Put the extra object back where it was originally
         {
             i.ToggleExaminable(true, i.originalPosition);
         }
+
+        if(itemCount == maxItemCount)
+        {
+            allItemsPlacedSound.Post(gameObject);
+            AllItemsPlaced?.Invoke();
+            Debug.Log("All items placed");
+        }
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         //Get reference to the position objects
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < maxItemCount; i++)
         {
             string name = ("Pos" + i); //Position objects are named Pos1, Pos2, Pos3...
             GameObject pos = GameObject.Find(name);
--- Assets/Scripts/PlayerHUDManager.cs
+++ Assets/Scripts/PlayerHUDManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,15 @@ public class PlayerHUDManager : MonoBehaviour
     private VisualElement Container;
     private Label abilityText;
     private VisualElement healthBar;
+    private VisualElement maxHealthBar;
     private void Awake()
     {
         var root     = GetComponent<UIDocument>().rootVisualElement;
         EntireScreen = root.Q<VisualElement>("EntireScreen");
         Container    = root.Q<VisualElement>("Prompts");
         abilityText  = root.Q<Label>("currentAbility");
-        healthBar = root.Q(className: "unity-progress-bar__progress");
+        healthBar    = root.Q(className: "unity-progress-bar__progress");
+        maxHealthBar = root.Q(className: "unity-progress-bar__container");
         
     }
 
@@ -70,26 +73,35 @@ public class PlayerHUDManager : MonoBehaviour
         
     }
 
-    void ShowDisplay()
-    {
+    void ShowDisplay() => EntireScreen.style.display = DisplayStyle.Flex;
+    void HideDisplay() => EntireScreen.style.display = DisplayStyle.None;
+    void AbilityChanged(string text) => abilityText.text = text;
 
-        EntireScreen.style.display = DisplayStyle.Flex;
 
-    }
+    private void UpdateHealth(HealthBarInfo healtbarInfo)
+    {   
+        float duration = 0.25f;
+        float maxHealth = maxHealthBar.style.width.value.value;
 
-    void HideDisplay()
-    {
-        EntireScreen.style.display = DisplayStyle.None;
-    }
 
-    void AbilityChanged(string text)
-    {
-        abilityText.text = text;
-    }
+        if (maxHealth < healtbarInfo.maxHealth)
+        {
+            //if the max is increasing have the container move faster that the health bar. 
+            float barDuration = .12f;
+            DOTween.To(() => maxHealthBar.style.width.value.value, y => maxHealthBar.style.width = Length.Percent(y), healtbarInfo.maxHealth, barDuration);
+        }
+        else if(maxHealth > healtbarInfo.maxHealth)
+        {
+            //If the max is decreasing then have the progress bar move faster than the container
+            float barDuration = .5f;
+            DOTween.To(() => maxHealthBar.style.width.value.value, y => maxHealthBar.style.width = Length.Percent(y), healtbarInfo.maxHealth, barDuration);
+        }
+        
+        // Tween the health bar width directly
+        DOTween.To(() => healthBar.style.width.value.value, x => healthBar.style.width = Length.Percent(x), healtbarInfo.currentHealth, duration).SetEase(Ease.OutCubic);
 
-    private void UpdateHealth(float value)
-    {
-        healthBar.style.width = Length.Percent(value);
+        // Log incoming health value
+        Debug.Log("Incoming Health: " + healtbarInfo.currentHealth);
     }
 
 }
